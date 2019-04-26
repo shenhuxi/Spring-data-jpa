aa6ba5a75b1a8e29b8e216a9f6123b518b215dba
zengpeng <1769411197@qq.com>
2019-04-26 17:49:15 +0800

    修改Bug

--- src/main/java/com/zpself/module/system/service/UserServiceImpl.java
+++ src/main/java/com/zpself/module/system/service/UserServiceImpl.java
@@ -3,16 +3,18 @@ package com.zpself.module.system.service;
 import com.zpself.jpa.repository.BaseRepository;
 import com.zpself.jpa.service.impl.BaseServiceImpl;
 import com.zpself.module.system.entity.User;
+import com.zpself.module.system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl extends BaseServiceImpl<User,Long> implements UserService {
-   /* @Autowired
-    UserRepository userRepository;*/
+   @Autowired
+   UserRepository userRepository;
 
     @Override
     public BaseRepository<User, Long> getCommonRepository() {
-        return null;
+        return userRepository;
     }
 
     @Override
